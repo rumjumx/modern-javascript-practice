ac6dc3446f7936f637d3b312001eec82c1d22957
P Roka <proka3131@outlook.com>
2025-10-31 21:25:53 +0530

    completed capitlize challenge

--- 01-variable-data-types/10-capitalize-challege/script.js
+++ 01-variable-data-types/10-capitalize-challege/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const myString = 'developer';
+let myNewString;
+
+// Solution 1:
+myNewString = myString[0].toUpperCase() + myString.slice(1);
+
+// Solution 2:
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+
+myNewString = `${myString.charAt(0).toUpperCase()}${myString.substring(1)}`;
+
+console.log(myNewString);
